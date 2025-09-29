3f5ca77a3356bffc6be83b57259369d413766348
Ammar Alhmoud <ammar.m.alhmoud@gmail.com>
2025-09-30 00:42:32 +0300

    add Redux and configure store for app-wide state

---
+++ src/store/appSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { type Project, type Task } from "../types/workspace";
+
+interface AppState {
+  isDarkMode: boolean;
+  projectModal: boolean | null;
+  projectsData: Project[] | undefined;
+  isTaskModal: boolean | null;
+  clickTask: Task | null;
+  isTaskDrawer: boolean | null;
+}
+
+const initialState: AppState = {
+  isDarkMode: localStorage.getItem("theme") === "dark",
+  projectModal: null,
+  projectsData: undefined,
+  isTaskModal: null,
+  clickTask: null,
+  isTaskDrawer: null,
+};
+
+const appSlice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    toggleTheme: (state) => {
+      state.isDarkMode = !state.isDarkMode;
+
+      if (state.isDarkMode) {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      }
+    },
+    setProjectModal(state, action: PayloadAction<boolean>) {
+      state.projectModal = action.payload;
+    },
+    setProjectsData(state, action: PayloadAction<Project[]>) {
+      state.projectsData = action.payload;
+    },
+    setTaskModal(state, action: PayloadAction<boolean>) {
+      state.isTaskModal = action.payload;
+    },
+    setClickTask(state, action: PayloadAction<Task | null>) {
+      state.clickTask = action.payload;
+    },
+    setTaskDrawer(state, action: PayloadAction<boolean>) {
+      state.isTaskDrawer = action.payload;
+    },
+  },
+});
+
+export const appActions = appSlice.actions;
+export default appSlice.reducer;
