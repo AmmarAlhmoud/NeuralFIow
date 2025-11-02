a88c1bac32281f3900f1f9ee98226445f2d4713a
Ammar Alhmoud <ammar.m.alhmoud@gmail.com>
2025-11-03 02:00:13 +0300

    prepare app to deploy on netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  define: {
+    ...Object.keys(process.env)
+      .filter((key) => key.startsWith("VITE_"))
+      .reduce<Record<string, string>>((acc, key) => {
+        acc[`import.meta.env.${key}`] = JSON.stringify(process.env[key]);
+        return acc;
+      }, {}),
+  },
 });
